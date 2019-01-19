2284af83bb57608b72bb56012ac1565730ae3ec4
Mikhail Kalinin <objque@gmail.com>
2019-01-20 00:58:56 +0300

    Fix api user validator

--- internal/api/validators/users.go
+++ internal/api/validators/users.go
@@ -1,9 +1,11 @@
 package validators
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func IsUserExits(w http.ResponseWriter, name string) error {
 	w.WriteHeader(http.StatusHTTPVersionNotSupported)
+	return fmt.Errorf("user '%s' not found", name)
 }
